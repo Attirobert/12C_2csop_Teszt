7f039330939ec905a66c7e95d228dfabbd7f128c
Attirobert <karobatti@gmail.com>
2024-09-25 12:00:07 +0200

    Teat

---
+++ Teszt_UnitTeszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Teszt_UnitTeszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Teszt_UnitTeszt")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("57b79a94-d769-41cf-8a35-cdc75abded5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Teszt_UnitTeszt/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Teszt_UnitTeszt
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange: Beállítások és a várt eredmény megadása
+            string vartEredm = "Helló Világ!";
+
+            // Act: Tesztelt metódus meghívása
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Teszt.Program.Main();
+                var kapottEredm = sw.ToString().Trim();
+
+                // Assert: Kiértékelés
+                Assert.AreEqual(vartEredm, kapottEredm);
+            }
+        }
+    }
+}
