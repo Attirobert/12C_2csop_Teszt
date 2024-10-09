4e37fd5274ba811f59bd7879961fe4e2db29d33f
Attirobert <karobatti@gmail.com>
2024-10-09 11:58:23 +0200

    OK

--- KorHenger/Kor.cs
+++ KorHenger/Kor.cs
@@ -19,9 +19,34 @@ namespace KorHenger
         public Kor() { }
         public Kor(double r)
         {
-
+            setDatas(r);
         }
 
+        private double kalkKerület(double r)
+        {
+            return 2 * r * Math.PI;
+        }
+
+        private double kalkTerület(double r)
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+
+        public double GetSugár() { return this.sugar; }
+        public double GetKerület() { return this.kerület; }
+        public double GetTerület() { return this.terület; }
+
+        public void SetSugar(double r)
+        {
+            setDatas(r);
+        }
+
+        private void setDatas(double r)
+        {
+            this.sugar = r;
+            this.kerület = kalkKerület(r);
+            this.terület = kalkTerület(r);
+        }
     }
 
 
