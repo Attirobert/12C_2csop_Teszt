5aa5c97d6171d18135d18c9acde489858d533e2a
Attirobert <karobatti@gmail.com>
2024-11-22 11:58:45 +0100

    try beépítés

--- KörHenger/Program.cs
+++ KörHenger/Program.cs
@@ -17,14 +17,41 @@ namespace KörHenger
             k1.SetTerület();
             k1.SetKerület();
 
-            kiír(k1);
+            //kiír(k1);
 
             // 2. példány
             Kör k2 = new Kör(58);
             k2.SetKerület();
             k2.SetTerület();
 
-            kiír(k2);
+            //kiír(k2);
+
+            // 1. henger 
+            try
+            {
+                Henger h1 = new Henger(15, 34);
+                kiírHenger(h1);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArithmeticException e) 
+            {
+                Console.WriteLine($"Aritmetikai hiba {e.Message}!");
+            }
+
+            try
+            {
+                Henger h2 = new Henger(-1,30);
+                kiírHenger(h2);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
 
             Console.ReadKey();
         }
@@ -32,6 +59,10 @@ namespace KörHenger
         private static void kiír(Kör k)
         {
             Console.WriteLine($"A {k.GetSugár()} sugarú kör kerülete: {k.GetKerület()}, területe: {k.GetTerület()}.");
+        }
+        private static void kiírHenger(Henger k)
+        {
+            Console.WriteLine($"A {k.GetSugár()} sugarú henger kerülete: {k.GetKerület()}, területe: {k.GetTerület()}, térfogata: {k.GetTérfogat()}.");
         }
     }
 }
