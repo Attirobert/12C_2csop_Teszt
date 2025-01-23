7476cc8b04827a427b6c0c8cd5222329d3ff590e
Attirobert <robatti@gmail.com>
2025-01-23 15:13:43 +0100

    Atvalt unit teszt elkezdve

---
+++ AtvaltOOP_UnitTeszt/Atvalt_ClassUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AtvaltOOP_UnitTeszt
+{
+    [TestClass]
+    public class Atvalt_ClassUnitTest   // Az Teszt osztály nevében jelzem, hogy melyik osztályt tesztelem
+    {
+        [TestMethod]
+        public void iSdecimal_TestMethod()  // A tesztmetódus nevében juelzem, hogy melyik metódust tesztelem
+        {
+            // Arrange - Tesztkörnyezet beállítása
+
+            // Act - A vizsgált metódus végrehajtása
+
+            // Arrange - Az eredmény kiértékelése
+        }
+    }
+}
---
+++ AtvaltOOP_UnitTeszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AtvaltOOP_UnitTeszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AtvaltOOP_UnitTeszt")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("23786e80-6e0b-4d02-aef3-c25a0e646f2f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
