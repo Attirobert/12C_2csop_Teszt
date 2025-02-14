e453ede7f4bdbcf391295a6370deeebc02d408de
Attirobert <karobatti@gmail.com>
2025-02-14 12:32:15 +0100

    Háromszög unit teszt feleléssel folytatás

--- Haromszogek_UnitTest/Haromszog_Class.cs
+++ Haromszogek_UnitTest/Haromszog_Class.cs
@@ -52,5 +52,39 @@ namespace Haromszogek_UnitTest
             //Assert - Teszt kiértékelése
             Assert.AreEqual(vartEredmeny , kapottEredmeny);
         }
+        [TestMethod]
+        public void isHaromszog_Test_False()
+        {
+            haromszog.setOldalak(1, 3, 5);
+            bool vartEredmeny=false;
+            bool kapottEredmeny;
+
+            kapottEredmeny=haromszog.isHaromszog();
+
+            // Assert.AreEqual(vartEredmeny, kapottEredmeny);
+            Assert.IsFalse(kapottEredmeny);
+        }
+        [TestMethod]
+        public void isEgyenloSzaru_Test_True()
+        {
+            //Arrange- Teszt környezet beállítása
+            haromszog.setOldalak(16, 16, 10);
+            bool vartEredmeny=true;
+            bool kapottEredmeny;
+            //Act -Teszt végrehajtása
+            kapottEredmeny = haromszog.isEgyenloSzaru();
+            //Assert-Teszt kiértékelése
+            Assert.AreEqual(vartEredmeny, kapottEredmeny);
+            
+        }
+        public void isEgyenloSzaru_Test_False()
+        {
+            haromszog.setOldalak(12, 16, 18);
+           
+            bool kapottEredmeny;
+            kapottEredmeny = haromszog.isEgyenloSzaru();
+            Assert.IsFalse(kapottEredmeny);
+        }
+            
     }
 }
